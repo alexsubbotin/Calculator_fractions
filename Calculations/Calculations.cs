2490ae26445ed3aa4c55f73b10d3391765e8cc6c
FilaCo <dvchizha@edu.hse.ru>
2018-04-10 12:56:44 +0500

    Ver 1.0 Реализованы основные операции, GCD, LCM, ToDecimal

--- Calculations/Calculations.cs
+++ Calculations/Calculations.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Calculations
 {
-    class Program
+    class Calculations
     {
-        static void Main(string[] args)
+        public static int LCM (int a, int b)
         {
+            return a * b / GCD(a,b);
+        }
+        public static int GCD (int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+        static Fraction Plus(Fraction a, Fraction b)
+        {
+            return a + b;
+        }
+        static Fraction Minus(Fraction a, Fraction b)
+        {
+            return a - b;
+        }
+        static Fraction Multiply(Fraction a, Fraction b)
+        {
+            return a * b;
+        }
+        static Fraction Divide(Fraction a, Fraction b)
+        {
+            return a / b;
+        }
+        static double ToDecimal(Fraction curFrac)
+        {
+            return (double)(curFrac.Numerator) / curFrac.Denumerator;
         }
     }
 }
