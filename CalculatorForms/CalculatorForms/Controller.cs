9a2107b3ec6ca02f738d8aa3d44b2961b64f4f7f
Alexey Subbotin <subbotin910@gmail.com>
2018-04-13 17:41:36 +0500

    Commented

--- CalculatorForms/CalculatorForms/Controller.cs
+++ CalculatorForms/CalculatorForms/Controller.cs
@@ -11,19 +11,27 @@ namespace CalculatorForms
     {
         public static string Calculate(string s, ref int globalIndex)
         {
-            
+            // The first element.
             string X = "";
+
+            // The second element.
             string Y = "";
+
+            // The operation between the elements.
             string operation = "";
 
+            // Current index in the string.
             int currentIndex = 0;
 
+            // While the current symbol is a digit / '(' / '.'
             while (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == '.')
             {
+                // If it't the '(' then send the rest to the Calculate method.
                 if (s[currentIndex] == '(')
                 {
                     X += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex), ref currentIndex);
                 }
+                // If it's a digit/ '.' then store it.
                 else
                 {
                     X += s[currentIndex];
@@ -31,45 +39,57 @@ namespace CalculatorForms
                 }
             }
 
+            // Store the symbol of an operation.
             operation += s[currentIndex];
+
+            // Going to the next symbol.
             currentIndex++;
 
+            // While the current symbol is a digit / '(' / ')' / '.'
             while (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == ')' || s[currentIndex] == '.')
             {
+                // If it's a digit / '.' then store it.
                 if (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '.')
                 {
                     Y += s[currentIndex];
                     currentIndex++;
                 }
 
+                // If it's the '(' then send the rest to the Calculate method.
                 if(s[currentIndex] == '(')
                 {
                     Y += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex), ref currentIndex);
                 }
 
+                // If it's the ')' then the Y is done.
                 if(s[currentIndex] == ')')
                 {
                     currentIndex++;
                 }
             }
 
+            // In the string which used the Calculate method we skip (currentindex + 1) letters.
             globalIndex += currentIndex;
 
+            // Operations.
+            string result = "";
             switch (operation)
             {
                 case "+":
-                    //buf =  X + Y;
+                    //result =  X + Y;
                     break;
                 case "-":
-                    //buf = X - Y;
+                    //result = X - Y;
                     break;
                 case "*":
-                    //buf = X * Y;
+                    //result = X * Y;
                     break;
                 case "/":
-                    //buf = X / Y;
+                    //result = X / Y;
                     break;
             }
+
+            return result;
         }
     }
 }
