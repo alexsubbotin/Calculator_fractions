dde6c02e33094b4a415d25038e124781f53ea3d6
Alexey Subbotin <subbotin910@gmail.com>
2018-04-13 17:30:59 +0500

    1st version of the calculating algorithm

--- CalculatorForms/CalculatorForms/Controller.cs
+++ CalculatorForms/CalculatorForms/Controller.cs
@@ -9,6 +9,67 @@ namespace CalculatorForms
     // Stores needed functions.
     class Controller
     {
+        public static string Calculate(string s, ref int globalIndex)
+        {
+            
+            string X = "";
+            string Y = "";
+            string operation = "";
 
+            int currentIndex = 0;
+
+            while (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == '.')
+            {
+                if (s[currentIndex] == '(')
+                {
+                    X += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex), ref currentIndex);
+                }
+                else
+                {
+                    X += s[currentIndex];
+                    currentIndex++;
+                }
+            }
+
+            operation += s[currentIndex];
+            currentIndex++;
+
+            while (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == ')' || s[currentIndex] == '.')
+            {
+                if (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '.')
+                {
+                    Y += s[currentIndex];
+                    currentIndex++;
+                }
+
+                if(s[currentIndex] == '(')
+                {
+                    Y += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex), ref currentIndex);
+                }
+
+                if(s[currentIndex] == ')')
+                {
+                    currentIndex++;
+                }
+            }
+
+            globalIndex += currentIndex;
+
+            switch (operation)
+            {
+                case "+":
+                    //buf =  X + Y;
+                    break;
+                case "-":
+                    //buf = X - Y;
+                    break;
+                case "*":
+                    //buf = X * Y;
+                    break;
+                case "/":
+                    //buf = X / Y;
+                    break;
+            }
+        }
     }
 }
