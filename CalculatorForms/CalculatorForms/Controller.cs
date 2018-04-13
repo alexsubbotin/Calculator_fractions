cf307731fe1853f71c0c916099c479c87e5040b5
Alexey Subbotin <subbotin910@gmail.com>
2018-04-13 18:27:27 +0500

    Bugs fixed

--- CalculatorForms/CalculatorForms/Controller.cs
+++ CalculatorForms/CalculatorForms/Controller.cs
@@ -23,72 +23,83 @@ namespace CalculatorForms
             // Current index in the string.
             int currentIndex = 0;
 
-            // While the current symbol is a digit / '(' / '.'
-            while (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == '.')
-            {
-                // If it's the '(' then send the rest to the Calculate method.
-                if (s[currentIndex] == '(')
-                {
-                    X += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex - 1), ref currentIndex);
-                }
-                // If it's a digit/ '.' then store it.
-                else
-                {
-                    X += s[currentIndex];
-                    currentIndex++;
-                }
-            }
-
-            // Store the symbol of an operation.
-            operation += s[currentIndex];
-
-            // Going to the next symbol.
-            currentIndex++;
-
-            // While the current symbol is a digit / '(' / ')' / '.'
-            while (currentIndex < s.Length && 
-                (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == ')' || s[currentIndex] == '.'))
-            {
-
-                // If it's the '(' then send the rest to the Calculate method.
-                if(s[currentIndex] == '(')
-                {
-                    Y += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex - 1), ref currentIndex);
-                }
-                else
-                {
-                    // If it's a digit '.' then store it.
-                    if (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '.')
-                    {
-                        Y += s[currentIndex];
-                    }
-
-                    currentIndex++;
-                }
-            }
-
-            // In the string which used the Calculate method we skip (currentindex + 1) symbols.
-            globalIndex += currentIndex + 1;
-
-            // Operations.
+            // The result.
             string result = "";
-            switch (operation)
+
+            bool Xfound = false;
+
+            while (currentIndex < s.Length)
             {
-                case "+":
-                    result =  (Convert.ToInt32(X) + Convert.ToInt32(Y)).ToString();
-                    break;
-                case "-":
-                    result = (Convert.ToInt32(X) - Convert.ToInt32(Y)).ToString();
-                    break;
-                case "*":
-                    result = (Convert.ToInt32(X) * Convert.ToInt32(Y)).ToString();
-                    break;
-                case "/":
-                    result = (Convert.ToInt32(X) / Convert.ToInt32(Y)).ToString();
-                    break;
+                // While the current symbol is a digit / '(' / '.'
+                while (!Xfound && 
+                    (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == '.'))
+                {
+                    // If it's the '(' then send the rest to the Calculate method.
+                    if (s[currentIndex] == '(')
+                    {
+                        X += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex - 1), ref currentIndex);
+                    }
+                    // If it's a digit/ '.' then store it.
+                    else
+                    {
+                        X += s[currentIndex];
+                        currentIndex++;
+                    }
+                }
+                Xfound = true;
+                // Store the symbol of an operation.
+                operation = s[currentIndex].ToString();
+
+                // Going to the next symbol.
+                currentIndex++;
+
+                // While the current symbol is a digit / '(' / ')' / '.'
+                while (currentIndex < s.Length &&
+                    (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '(' || s[currentIndex] == ')' || s[currentIndex] == '.'))
+                {
+
+                    // If it's the '(' then send the rest to the Calculate method.
+                    if (s[currentIndex] == '(')
+                    {
+                        Y += Calculate(s.Substring(currentIndex + 1, s.Length - currentIndex - 1), ref currentIndex);
+                    }
+                    else
+                    {
+                        // If it's a digit '.' then store it.
+                        if (Char.IsDigit(s[currentIndex]) || s[currentIndex] == '.')
+                        {
+                            Y += s[currentIndex];
+                        }
+
+                        currentIndex++;
+                    }
+                }
+
+                // In the string which used the Calculate method we skip (currentindex + 1) symbols.
+                globalIndex += currentIndex + 1;
+
+                // Operations.
+                switch (operation)
+                {
+                    case "+":
+                        result = (Convert.ToInt32(X) + Convert.ToInt32(Y)).ToString();
+                        break;
+                    case "-":
+                        result = (Convert.ToInt32(X) - Convert.ToInt32(Y)).ToString();
+                        break;
+                    case "*":
+                        result = (Convert.ToInt32(X) * Convert.ToInt32(Y)).ToString();
+                        break;
+                    case "/":
+                        result = (Convert.ToInt32(X) / Convert.ToInt32(Y)).ToString();
+                        break;
+                }
+
+                X = result;
+                Y = "";
             }
 
-            return result;
+            return X;
         }
     }
 }
