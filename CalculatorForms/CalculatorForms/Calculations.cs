e5ea111d012e5af44d2a318e437aae298c25256a
Alexey Subbotin <subbotin910@gmail.com>
2018-04-14 15:53:45 +0500

    Data and calculation classes added

---
+++ CalculatorForms/CalculatorForms/Calculations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorForms
+{
+    public class Calculations
+    {
+        public static Fraction Plus(Fraction a, Fraction b)
+        {
+            return a + b;
+        }
+        public static Fraction Minus(Fraction a, Fraction b)
+        {
+            return a - b;
+        }
+        public static Fraction Multiply(Fraction a, Fraction b)
+        {
+            return a * b;
+        }
+        public static Fraction Divide(Fraction a, Fraction b)
+        {
+            return a / b;
+        }
+        public static double ToDecimal(Fraction curFrac)
+        {
+            return (double)(curFrac.Numerator) / curFrac.Denominator;
+        }
+        private static int[] GetNumeratorAndDenum(double value)
+        {
+            int denum = 1, numer = 1;
+            int i = 0;
+            while (Math.Abs(value* Math.Pow(10, i) - numer) > 0.000000001 && i<9)
+            {
+                i++;
+                numer = (int)(value * Math.Pow(10,i));
+            }
+            denum = (int)Math.Pow(10, i);
+            return new int[]{ numer, denum};
+        }
+        public static Fraction ToSimpleFraction(double value)
+        {
+            int[] pairNum_Denum = GetNumeratorAndDenum(value);
+            return new Fraction(pairNum_Denum[0], pairNum_Denum[1]);
+        }
+    }
+}
---
+++ CalculatorForms/CalculatorForms/Fractions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorForms
+{
+    public class Fraction
+    {
+        private int numerator;
+        public int Numerator
+        {
+            get
+            {
+                return numerator;
+            }
+            set
+            {
+                numerator = value;
+            }
+        }
+        private int denominator;
+        public int Denominator
+        {
+            get
+            {
+                return denominator;
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    return;
+                }
+                denominator = value;
+            }
+        }
+        public Fraction()
+        {
+            Numerator = -1;
+            Denominator = -1;
+        }
+        public Fraction(int _num, int _den)
+        {
+            Numerator = _num;
+            Denominator = _den;
+            Simplify();
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+                Fraction newFract = new Fraction(a.Numerator + b.Numerator, a.Denominator);
+                newFract.Simplify();
+                return newFract;
+            }
+            Fraction tmp = new Fraction();
+            int newDen = LCM(a.Denominator, b.Denominator);
+            tmp.Denominator = newDen;
+            tmp.Numerator = a.Numerator * (newDen / a.Denominator) + b.Numerator * (newDen / b.Denominator);
+            tmp.Simplify();
+            return tmp;
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            if (a.Denominator == b.Denominator)
+            {
+                Fraction newFract = new Fraction(a.Numerator - b.Numerator, a.Denominator);
+                newFract.Simplify();
+                return newFract;
+            }
+            Fraction tmp = new Fraction();
+            int newDen = LCM(a.Denominator, b.Denominator);
+            tmp.Denominator = newDen;
+            tmp.Numerator = a.Numerator * (newDen / a.Denominator) - b.Numerator * (newDen / b.Denominator);
+            tmp.Simplify();
+            return tmp;
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            Fraction newFract = new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+            newFract.Simplify();
+            return newFract;
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            b = new Fraction(b.Denominator, b.Numerator);
+            return a * b;
+        }
+        private void Simplify()
+        {
+            int div = GCD(Math.Abs(Numerator), Math.Abs(Denominator));
+            Numerator /= div;
+            Denominator /= div;
+        }
+        private static int GCD(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return GCD(b, a % b);
+        }
+        private static int LCM(int a, int b)
+        {
+            return a * b / GCD(a, b);
+        }
+        public override string ToString()
+        {
+            return Numerator + "/" + Denominator;
+        }
+    }
+}
